0b92b89dea6f8eb1e99c022dbd3141c7654fc184
Seung-Jong Oh <tmdwhd319@gmail.com>
2024-03-16 02:14:05 +0900

    server actions, new FileReader()

--- app/meals/share/page.tsx
+++ app/meals/share/page.tsx
@@ -1,3 +1,68 @@
+import ImagePicker from "@/components/meals/image-picker";
+import styles from "./page.module.css";
+
 export default function ShareMealPage() {
-  return <h1>share Meal</h1>;
+  // 서버 액션 async 무조건 붙여야함.
+  // 원래 form의 action 속성은 백엔드 경로로 설정하는데
+  // 이렇게 설정하면 <form action={shareMeal}> 가능
+  const shareMeal = async (formData: FormData) => {
+    "use server";
+
+    const meal = {
+      title: formData.get("title"),
+      summary: formData.get("summary"),
+      instructions: formData.get("instructions"),
+      image: formData.get("image"),
+      creator: formData.get("name"),
+      creator_email: formData.get("email"),
+    };
+
+    console.log(meal);
+  };
+
+  return (
+    <>
+      <header className={styles.header}>
+        <h1>
+          Share your <span className={styles.highlight}>favorite meal</span>
+        </h1>
+        <p>Or any other meal you feel needs sharing!</p>
+      </header>
+      <main className={styles.main}>
+        <form className={styles.form} action={shareMeal}>
+          <div className={styles.row}>
+            <p>
+              <label htmlFor="name">Your name</label>
+              <input type="text" id="name" name="name" required />
+            </p>
+            <p>
+              <label htmlFor="email">Your Email</label>
+              <input type="email" id="email" name="email" required />
+            </p>
+          </div>
+          <p>
+            <label htmlFor="title">Title</label>
+            <input type="text" id="title" name="title" required />
+          </p>
+          <p>
+            <label htmlFor="summary">Short Summary</label>
+            <input type="text" id="summary" name="summary" required />
+          </p>
+          <p>
+            <label htmlFor="instructions">Instructions</label>
+            <textarea
+              id="instructions"
+              name="instructions"
+              rows={10}
+              required
+            ></textarea>
+          </p>
+          <ImagePicker label="Your image" name="image" />
+          <p className={styles.actions}>
+            <button type="submit">Share Meal</button>
+          </p>
+        </form>
+      </main>
+    </>
+  );
 }
---
+++ components/meals/image-picker.tsx
@@ -0,0 +1,56 @@
+"use client";
+
+import styles from "@/styles/image-picker.module.css";
+import { StaticImport } from "next/dist/shared/lib/get-img-props";
+import Image from "next/image";
+import { useRef, useState } from "react";
+
+export default function ImagePicker({ label, name }) {
+  const [pickedImage, setPickedImage] = useState<string | ArrayBuffer>();
+  const imageInputRef = useRef<HTMLInputElement>();
+
+  const handlePicker = () => imageInputRef.current.click();
+
+  const handleImageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files[0];
+    if (!file) {
+      setPickedImage(null);
+      return;
+    }
+
+    const fileReader = new FileReader();
+    fileReader.onload = () => setPickedImage(fileReader.result);
+    fileReader.readAsDataURL(file);
+  };
+
+  return (
+    <div className={styles.picker}>
+      <label htmlFor={name}>{label}</label>
+      <div className={styles.controls}>
+        <div className={styles.preview}>
+          {!pickedImage && <p>No image picked yet.</p>}
+          {pickedImage && (
+            <Image
+              src={pickedImage as string}
+              alt="The image selected by user."
+              fill
+            />
+          )}
+        </div>
+        <input
+          className={styles.input}
+          type="file"
+          id={name}
+          accept="image/png, image/jpeg"
+          name={name}
+          ref={imageInputRef}
+          onChange={handleImageChange}
+          required
+        />
+        <button className={styles.button} type="button" onClick={handlePicker}>
+          Pick an Image
+        </button>
+      </div>
+    </div>
+  );
+}
