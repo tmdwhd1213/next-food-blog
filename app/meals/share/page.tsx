8937fe864a6995d5da5e008108f6322196c7eb4d
Seung-Jong Oh <tmdwhd319@gmail.com>
2024-03-16 03:41:17 +0900

    server action responses & useFormState

--- app/meals/share/page.tsx
+++ app/meals/share/page.tsx
@@ -1,9 +1,13 @@
+"use client";
+
 import ImagePicker from "@/components/meals/image-picker";
 import styles from "./page.module.css";
 import { shareMeal } from "@/lib/actions";
 import MealsFormSubmit from "@/components/meals/meals-form-submit";
+import { useFormState } from "react-dom";
 
 export default function ShareMealPage() {
+  const [state, formActions] = useFormState(shareMeal, { message: null });
   // 서버 액션 async 무조건 붙여야함.
   // 원래 form의 action 속성은 백엔드 경로로 설정하는데
   // 이렇게 설정하면 <form action={shareMeal}> 가능
@@ -17,7 +21,7 @@ export default function ShareMealPage() {
         <p>Or any other meal you feel needs sharing!</p>
       </header>
       <main className={styles.main}>
-        <form className={styles.form} action={shareMeal}>
+        <form className={styles.form} action={formActions}>
           <div className={styles.row}>
             <p>
               <label htmlFor="name">Your name</label>
@@ -46,6 +50,7 @@ export default function ShareMealPage() {
             ></textarea>
           </p>
           <ImagePicker label="Your image" name="image" />
+          {state.message && <p>{state.message}</p>}
           <p className={styles.actions}>
             <MealsFormSubmit />
           </p>
--- lib/actions.tsx
+++ lib/actions.tsx
@@ -4,7 +4,11 @@ import slugify from "slugify";
 import { saveMeal } from "./meals";
 import { redirect } from "next/navigation";
 
-export async function shareMeal(formData) {
+function isInvalidText(text) {
+  return !text || text.trim() === "";
+}
+
+export async function shareMeal(prevState, formData) {
   const meal = {
     title: formData.get("title"),
     summary: formData.get("summary"),
@@ -13,6 +17,25 @@ export async function shareMeal(formData) {
     creator: formData.get("name"),
     creator_email: formData.get("email"),
   };
+
+  // html에 required로 필수 입력란을 만들었지만,
+  // F12 누르고 사용자가 지우면 백엔드에 빈 상태로 보내게 된다.
+  // 이를 막기위해 백엔드에서도 유효성 검사를 진행해야함.
+  if (
+    isInvalidText(meal.title) ||
+    isInvalidText(meal.summary) ||
+    isInvalidText(meal.creator) ||
+    isInvalidText(meal.creator_email) ||
+    isInvalidText(meal.image) ||
+    isInvalidText(meal.instructions) ||
+    !meal.creator_email.includes("@") ||
+    !meal.image ||
+    meal.image.size === 0
+  ) {
+    return {
+      message: "Invalid input.",
+    };
+  }
 
   // console.log(meal);
   await saveMeal(meal);
