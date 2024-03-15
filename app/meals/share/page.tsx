ed6aeb09c26da95ed47dc999e3784e024c056d42
Seung-Jong Oh <tmdwhd319@gmail.com>
2024-03-16 02:21:06 +0900

    seperate server actions

--- app/meals/share/page.tsx
+++ app/meals/share/page.tsx
@@ -1,24 +1,11 @@
 import ImagePicker from "@/components/meals/image-picker";
 import styles from "./page.module.css";
+import { shareMeal } from "@/lib/actions";
 
 export default function ShareMealPage() {
   // 서버 액션 async 무조건 붙여야함.
   // 원래 form의 action 속성은 백엔드 경로로 설정하는데
   // 이렇게 설정하면 <form action={shareMeal}> 가능
-  const shareMeal = async (formData: FormData) => {
-    "use server";
-
-    const meal = {
-      title: formData.get("title"),
-      summary: formData.get("summary"),
-      instructions: formData.get("instructions"),
-      image: formData.get("image"),
-      creator: formData.get("name"),
-      creator_email: formData.get("email"),
-    };
-
-    console.log(meal);
-  };
 
   return (
     <>
